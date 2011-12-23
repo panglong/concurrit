05cd3e2a425db3821a1cded99e19054eff4641c1
tayfunelmas <tayfunelmas@gmail.com>
2011-12-22 23:32:18 -0800

    Added a thr-mod test.

---
+++ tests/modular/test_modular.cpp
@@ -0,0 +1,119 @@
+
+#include "counit.h"
+#include "math.h"
+#include <assert.h>
+
+using namespace counit;
+
+struct Counter {
+	int x;
+	int l;
+
+	Counter() {
+		x = 0;
+		l = 0;
+	}
+
+	void increment() {
+
+		while(true) {
+			int t = YIELD_READ("L1", x);
+			int k = t + 1;
+
+			//lock();
+			if(YIELD_READ("L2", x) == t) {
+				YIELD_WRITE("L3", x) = k;
+//				unlock();
+				break;
+			}
+			//unlock();
+		}
+
+	}
+
+	void lock() {
+		ASSUME(YIELD_READ("lock1", l) == 0);
+		WRITE_YIELD("lock2", l) = 1;
+	}
+
+	void unlock() {
+		WRITE_YIELD("unlock", l) = 0;
+	}
+
+	int get() {
+		return x;
+	}
+};
+
+
+void* increment_thread(void* p) {
+	Counter* counter = static_cast<Counter*>(p);
+
+	counter->increment();
+
+	return NULL;
+}
+
+class CounterScenario : public ThreadModularScenario {
+public:
+
+	CounterScenario(const char* name) : ThreadModularScenario(name) {}
+	~CounterScenario() {}
+
+	virtual void SetUp() {
+		counter = new Counter();
+	}
+
+	virtual void TearDown() {
+		delete counter;
+	}
+
+protected:
+	Counter* counter;
+
+};
+
+#define COUNTER (this->counter)
+
+/************************************************************************/
+
+class Scenario0 : public CounterScenario {
+public:
+	Scenario0() : CounterScenario("SearchTest0") {}
+
+	void TestCase() {
+
+		coroutine_t t1 = CreateThread("t1", increment_thread, COUNTER);
+		coroutine_t t2 = CreateThread("t2", increment_thread, COUNTER);
+		coroutine_t t3 = CreateThread("t3", increment_thread, COUNTER);
+		coroutine_t t4 = CreateThread("t4", increment_thread, COUNTER);
+
+		int i = 0;
+		UNTIL_ALL_END {
+			UNTIL_STAR()->TRANSFER_STAR();
+			++i;
+		}
+
+		ASSERT(COUNTER->get() == i);
+
+		printf("Counter: %d\n", COUNTER->get());
+	}
+};
+
+/************************************************************************/
+
+/************************************************************************/
+
+int main(int argv, char** argc) {
+
+	BeginCounit();
+
+	Suite suite;
+
+	suite.AddScenario(new Scenario0());
+	suite.RunAll();
+
+	EndCounit();
+
+	return 0;
+}
