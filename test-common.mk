8061a8c7d375e5f5c0d40094b37b883cc4f407c8
tayfuelmas <tayfunelmas@gmail.com>
2012-02-09 17:00:33 -0800

    Edited makefiles to add PROGRAM_INC_FLAGS and PROGRAM_LIB_FLAGS.

--- test-common.mk
+++ test-common.mk
@@ -1,12 +1,16 @@
 include $(CONCURRIT_HOME)/common.mk
 
-# variables to set: TARGET, SRCS
+# variables to set: TARGET, SRCS, optional: HEADERS
 
 all: $(TARGET)
 
-$(TARGET): $(SRCS) 
-	g++ $(CONCURRIT_TEST_INC_FLAGS) $(CONCURRIT_TEST_LIB_FLAGS) -gdwarf-2 -O0 -Wall -o $(TARGET) $^
-	g++ $(CONCURRIT_TEST_INC_FLAGS) $(CONCURRIT_TEST_LIB_FLAGS) -gdwarf-2 -O0 -Wall -c -o $(TARGET).o $^
+TEST_INC_FLAGS=$(PROGRAM_INC_FLAGS) $(CONCURRIT_TEST_INC_FLAGS)
+TEST_LIB_FLAGS=$(PROGRAM_LIB_FLAGS) $(CONCURRIT_TEST_LIB_FLAGS)
+TEST_FLAGS=$(TEST_INC_FLAGS) $(TEST_LIB_FLAGS) -gdwarf-2 -O0 -Wall
+
+$(TARGET): $(SRCS) $(HEADERS)
+	g++ $(TEST_FLAGS) -o $(TARGET) $^
+	g++ $(TEST_FLAGS) -c -o $(TARGET).o $^
 	ar rcs $(TARGET).a $(TARGET).o
 	
 clean:
