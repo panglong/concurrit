df05bf627d309232d3400e9cb8d9f11795a031f1
tayfunelmas <tayfunelmas@gmail.com>
2012-02-18 23:40:20 -0800

    Added radbench directory and build-all script.

---
+++ bench/radbench/build-all.sh
@@ -0,0 +1,20 @@
+bug1/scripts/build-ff.sh
+bug1/scripts/build-testharness.sh
+
+bug2/scripts/build-js.sh
+bug1/scripts/build-testharness.sh
+
+bug3/scripts/build-js.sh 1.8
+bug3/scripts/build-testharness.sh
+
+bug4/scripts/build-nspr.sh default
+bug4/scripts/build-testharness.sh
+
+bug5/scripts/build-nspr.sh default
+bug5/scripts/build-testharness.sh
+
+bug6/scripts/build-nspr.sh
+bug6/scripts/build-testharness.sh
+
+bug8/scripts/build-memcached.sh
+bug8/scripts/build-testharness.sh
