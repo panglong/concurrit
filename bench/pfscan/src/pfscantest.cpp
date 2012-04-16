8eabd23e2106e29729a96c3f5cbcf7ae96d93380
tayfuelmas <tayfunelmas@gmail.com>
2012-04-16 13:27:48 -0700

    Committing test case of pfscan.

--- bench/pfscan/src/pfscantest.cpp
+++ bench/pfscan/src/pfscantest.cpp
@@ -19,41 +19,15 @@ CONCURRIT_BEGIN_TEST(MyScenario, "My scenario")
 		EXISTS(t1, IN_FUNC(fg), "T1");
 		EXISTS(t2, NOT(t1) && IN_FUNC(fg), "T2");
 
-//		WHILE_STAR {
+		WHILE_STAR {
 
-			RUN_UNTIL(STEP(t1), ENTERS(fg), __, "pqueue_get by T1 (1)");
-
-			RUN_UNTIL(STEP(t1), AT_PC(42), __, "pqueue_get by T1");
+			RUN_UNTIL(STEP(t1), AT_PC(42), __, "pqueue_get starts by T1");
 
 	//		RUN_UNTIL(NOT(t1), RETURNS2(fg, t2), __, "pqueue_get by T2");
 			RUN_UNTIL(STEP(t2), RETURNS(fg), __, "pqueue_get by T2");
 
-			RUN_UNTIL(STEP(t1), RETURNS(fg), __, "pqueue_get by T1 (2)");
-//		}
-
-
-			RUN_UNTIL(STEP(t1), ENTERS(fg), __, "pqueue_get by T1 (1)");
-			RUN_UNTIL(STEP(t1), AT_PC(42), __, "pqueue_get by T1");
-
-				//		RUN_UNTIL(NOT(t1), RETURNS2(fg, t2), __, "pqueue_get by T2");
-						RUN_UNTIL(STEP(t2), RETURNS(fg), __, "pqueue_get by T2");
-
-						RUN_UNTIL(STEP(t1), RETURNS(fg), __, "pqueue_get by T1 (2)");
-
-						RUN_UNTIL(STEP(t1), AT_PC(42), __, "pqueue_get by T1");
-
-							//		RUN_UNTIL(NOT(t1), RETURNS2(fg, t2), __, "pqueue_get by T2");
-									RUN_UNTIL(STEP(t2), RETURNS(fg), __, "pqueue_get by T2");
-
-									RUN_UNTIL(STEP(t1), RETURNS(fg), __, "pqueue_get by T1 (2)");
-
-									RUN_UNTIL(STEP(t1), ENTERS(fg), __, "pqueue_get by T1 (1)");
-									RUN_UNTIL(STEP(t1), AT_PC(42), __, "pqueue_get by T1");
-
-										//		RUN_UNTIL(NOT(t1), RETURNS2(fg, t2), __, "pqueue_get by T2");
-												RUN_UNTIL(STEP(t2), RETURNS(fg), __, "pqueue_get by T2");
-
-												RUN_UNTIL(STEP(t1), RETURNS(fg), __, "pqueue_get by T1 (2)");
+			RUN_UNTIL(STEP(t1), RETURNS(fg), __, "pqueue_get ends by T1");
+		}
 	}
 
 CONCURRIT_END_TEST(MyScenario)
