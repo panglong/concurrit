51be9ab3edd811177d4d9a8aaa8ce412055d963f
tayfunelmas <tayfunelmas@gmail.com>
2012-04-22 21:45:53 -0700

    Modified compile_bench script to be able to compile all benchmarks using the "all" option.

--- scripts/compile_bench.sh
+++ scripts/compile_bench.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
+MAKEBENCH() {
+	make -C $BENCHDIR clean
+	make -C $BENCHDIR shared
+	make -C $BENCHDIR all
+}
+
 ARGS=( $@ )
 # get and remove name of the benchmark from arguments
 BENCH=${ARGS[0]}
 echo "Compiling $BENCH"
 
-BENCHDIR=$CONCURRIT_HOME/bench/$BENCH
-
-make -C $BENCHDIR clean
-make -C $BENCHDIR shared
-make -C $BENCHDIR all
+if [ $BENCH != "all" ];
+then
+	BENCHDIR=$CONCURRIT_HOME/bench/$BENCH
+	MAKEBENCH
+else
+	BENCHROOTS=$CONCURRIT_HOME/bench/*
+	for BENCHDIR in $BENCHROOTS
+	do
+		echo "Compiling $BENCHDIR"
+		MAKEBENCH
+	done
+fi
