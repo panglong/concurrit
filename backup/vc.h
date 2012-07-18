40376054f0ae29b2245b35e4f22273f6b791c54a
tayfunelmas <tayfunelmas@gmail.com>
2012-07-18 13:12:35 -0700

    First commit for removing yield points, move the old files to backup directory.

--- backup/vc.h
+++ backup/vc.h
@@ -31,111 +31,30 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-/************************************************/
 
 #ifndef VC_H_
 #define VC_H_
 
-#include <iostream>
-#include <iomanip>
-#include <fstream>
+#include "common.h"
 
-#include <vector>
-#include <ctime>
-#include <map>
-
-/************************************************/
-
-typedef unsigned int vctime_t;
+namespace concurrit {
 
 typedef std::map<THREADID,vctime_t> VC;
 typedef std::map<ADDRINT,VC> VCMAP;
 
-/************************************************/
+extern VC vc_get_vc(VCMAP& m, ADDRINT k);
+extern void vc_set_vc(VCMAP& m, ADDRINT k, VC vc);
 
-VC vc_get_vc(VCMAP& m, ADDRINT k);
-void vc_set_vc(VCMAP& m, ADDRINT k, VC vc);
-void vc_clear(VC& v);
-vctime_t vc_get(VC& vc, THREADID t);
-void vc_set(VC& vc, THREADID t, vctime_t c);
-void vc_inc(VC& vc, THREADID t);
-VC vc_cup(VC& vc1, VC& vc2);
-bool vc_leq(VC& vc1, VC& vc2, THREADID t);
-bool vc_leq_all(VC& vc1, VC& vc2);
+extern void vc_clear(VC& v);
+extern vctime_t vc_get(VC& vc, THREADID t);
+extern void vc_set(VC& vc, THREADID t, vctime_t c);
 
-/************************************************/
+extern void vc_inc(VC& vc, THREADID t);
+extern VC vc_cup(VC& vc1, VC& vc2);
 
-inline VC vc_get_vc(VCMAP& m, ADDRINT k) {
-	VCMAP::iterator it = m.find(k);
-	if(it != m.end()) {
-		return it->second;
-	}
-	return VC();
-}
+extern bool vc_leq(VC& vc1, VC& vc2, THREADID t);
+extern bool vc_leq_all(VC& vc1, VC& vc2);
 
-inline void vc_set_vc(VCMAP& m, ADDRINT k, VC vc) {
-	m[k] = vc;
-}
-
-void vc_clear(VC& v) {
-	v.clear();
-}
-
-inline vctime_t vc_get(VC& vc, THREADID t) {
-	VC::iterator it = vc.find(t);
-	if(it != vc.end()) {
-		vctime_t c = it->second;
-		assert(c > 0);
-		return c;
-	}
-	return 0; // rest is 0
-}
-
-inline void vc_set(VC& vc, THREADID t, vctime_t c) {
-	if(c > 0) {
-		vc[t] = c;
-	} else {
-		vc.erase(t);
-	}
-}
-
-inline void vc_inc(VC& vc, THREADID t) {
-	VC _vc_ = vc;
-	vctime_t c = vc_get(_vc_, t);
-	_vc_[t] = c + 1;
-}
-
-inline VC vc_cup(VC& vc1, VC& vc2) {
-	VC _vc_ = vc1;
-	for (VC::iterator it=vc2.begin() ; it != vc2.end(); it++ ) {
-		THREADID t = it->first;
-		vctime_t c1 = vc_get(_vc_, t);
-		vctime_t c2 = it->second;
-		if(c2 > c1) {
-			_vc_[t] = c2;
-		}
-	}
-	return _vc_;
-}
-
-inline bool vc_leq(VC& vc1, VC& vc2, THREADID t) {
-	vctime_t c1 = vc_get(vc1, t);
-	vctime_t c2 = vc_get(vc2, t);
-	return c1 <= c2;
-}
-
-inline bool vc_leq_all(VC& vc1, VC& vc2) {
-	for (VC::iterator it=vc2.begin() ; it != vc2.end(); it++ ) {
-		THREADID t = it->first;
-		vctime_t c1 = vc_get(vc1, t);
-		vctime_t c2 = it->second;
-		if(c1 > c2) {
-			return false;
-		}
-	}
-	return true;
-}
-
-/************************************************/
+} // end namespace
 
 #endif // VC_H_
