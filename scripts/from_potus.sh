6197b3d51e8d803f24a28d3a82d5b058047380c0
tayfunelmas <tayfunelmas@gmail.com>
2012-07-24 03:57:52 -0700

    Changed the install root for boost to $CONCURRIT_TPDIR.

--- scripts/from_potus.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rsync --exclude='glog**' --exclude='gtest**' --exclude='pin/**' -e ssh --numeric-ids --quiet --ignore-errors --archive --delete-during --force potus.cs.berkeley.edu:/home/elmas/concurrit/ $CONCURRIT_HOME/ 
