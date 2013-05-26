b0e932ccfa5ac3b6eb31c5686ae65d00fca05efa
tayfunelmas <tayfunelmas@gmail.com>
2013-05-25 17:53:57 -0700

    -injection child to the pintool runner.

--- scripts/run_pintool.sh
+++ scripts/run_pintool.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PIN_ARGS="-mt 1 -inline -follow_execv -separate_memory -slow_asserts"
+PIN_ARGS="-mt 1 -inline -follow_execv -separate_memory -slow_asserts -injection child"
 
 PINTOOL_ARGS="-skip_int3"
 PINTOOL_ARGS="$PINTOOL_ARGS -finfile $CONCURRIT_HOME/work/finfile.txt"
@@ -8,7 +8,6 @@ PINTOOL_ARGS="$PINTOOL_ARGS -log_file $CONCURRIT_HOME/work/pinlogfile.txt"
 PINTOOL_ARGS="$PINTOOL_ARGS -filtered_images_file $CONCURRIT_HOME/work/filtered_images.txt"
 PINTOOL_ARGS="$PINTOOL_ARGS -track_func_calls 0"
 PINTOOL_ARGS="$PINTOOL_ARGS -inst_top_level 0"
-PINTOOL_ARGS="$PINTOOL_ARGS -injection child"
 
 PROGRAM_ARGS="$@"
 
