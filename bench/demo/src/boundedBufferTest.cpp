21a0a787d36a7cccfc56cfacb19fee0721cb6248
tayfunelmas <tayfunelmas@gmail.com>
2013-05-29 21:32:21 -0700

    Demo updates.

--- bench/demo/src/boundedBufferTest.cpp
+++ bench/demo/src/boundedBufferTest.cpp
@@ -12,19 +12,19 @@ CONCURRIT_BEGIN_TEST(BBScenario, "Bounded buffer scenario")
 	TESTCASE() {
 		MAX_WAIT_TIME(0);
 
-		FUNCT(bounded_buf_get);
-		FUNCT(bounded_buf_put);
+		FUNCT(producer_routine);
+		FUNCT(consumer_routine);
 
 		TVAR(P1);
 		TVAR(P2);
 		TVAR(C1);
 		TVAR(C2);
 
-		WAIT_FOR_DISTINCT_THREADS((P1, P2), ENTERS(bounded_buf_put), "Wait for 2 producers.");
+		WAIT_FOR_DISTINCT_THREADS((P1, P2), ENTERS(producer_routine), "Wait for 2 producers.");
 
-		WAIT_FOR_DISTINCT_THREADS((C1, C2), ENTERS(bounded_buf_get), "Wait for 2 consumers.");
+		WAIT_FOR_DISTINCT_THREADS((C1, C2), ENTERS(consumer_routine), "Wait for 2 consumers.");
 
-		MAX_WAIT_TIME(USECSPERSEC);
+		MAX_WAIT_TIME(3*USECSPERSEC);
 
 		WHILE (!ALL_ENDED(P1, P2, C1, C2)) {
 
@@ -32,7 +32,7 @@ CONCURRIT_BEGIN_TEST(BBScenario, "Bounded buffer scenario")
 
 			CHOOSE_THREAD_BACKTRACK(t, (P1, P2, C1, C2), PTRUE, "Select a thread to execute.");
 
-			RUN_THREAD_THROUGH(t, ENTERS() || RETURNS() || HITS_MANUAL_PC() || ENDS(), "Run t until any event.");
+			RUN_THREAD_THROUGH(t, READS() || WRITES() || CALLS() || ENTERS() || RETURNS() || ENDS(), "Run t until any event.");
 		}
 	}
 
